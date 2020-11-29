fbc564e01f145d5ddab252abee7e871b367bee7a
Maikon Canuto <maikoncanuto@gmail.com>
2020-11-28 21:40:24 -0300

    add docker-compose

---
+++ backend/orchestrator-elementary/orch-elementary-user/src/main/java/com/github/maikoncanuto/oeu/OrchElementaryUserApplication.java
@@ -0,0 +1,13 @@
+package com.github.maikoncanuto.oeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchElementaryUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchElementaryUserApplication.class, args);
+	}
+
+}
---
+++ backend/orchestrator-elementary/orch-elementary-user/src/test/java/com/github/maikoncanuto/oeu/OrchElementaryUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maikoncanuto.oeu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchElementaryUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
